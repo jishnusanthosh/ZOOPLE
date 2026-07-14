4a9961f8d863024afb3d66230a262255b3c1d9f1
Jishnu Santhosh <jishnusanthosh321@gmail.com>
2026-07-14 13:55:36 +0530

    begginer level activites completed

---
+++ JAVASCRIPT/FUNCTION/Activities – Functions/Intermediate.js
@@ -0,0 +1,19 @@
+
+//Activity 11: Function Expression
+
+
+
+// const cube = function(num) {
+//     return num ** 3;
+// };
+
+// console.log(cube(3));
+
+//Activity 12: Arrow Function
+// const areaOfRectangle = (length, width) => {
+//     return length * width;
+// };
+
+// console.log(areaOfRectangle(10, 5));
+
+//Activity 13: Palindrome Checker
