2a36990156ca7db2822cc2005674dbbd9a375ae9
Jishnu Santhosh <jishnusanthosh321@gmail.com>
2026-07-14 13:57:32 +0530

    intermediate level  3 completed

--- JAVASCRIPT/FUNCTION/Activities – Functions/Intermediate.js
+++ JAVASCRIPT/FUNCTION/Activities – Functions/Intermediate.js
@@ -17,3 +17,21 @@
 // console.log(areaOfRectangle(10, 5));
 
 //Activity 13: Palindrome Checker
+// function isPalindrome(word) {
+//     let reversed = "";
+
+//     for (let i = word.length - 1; i >= 0; i--) {
+//         reversed += word[i];
+//     }
+
+//     if (word === reversed) {
+//         return "Palindrome";
+//     } else {
+//         return "Not Palindrome";
+//     }
+// }
+
+// console.log(isPalindrome("MADAM"));
+// console.log(isPalindrome("HELLO"));
+
+Activity 14: Grade Calculator
