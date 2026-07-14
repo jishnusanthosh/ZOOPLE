e33e09dad5d24996557642f442e72be187c39ee9
Jishnu Santhosh <jishnusanthosh321@gmail.com>
2026-07-14 11:55:17 +0530

    dom

---
+++ DOM/INTRODUCTION/script.js
@@ -0,0 +1,9 @@
+let heading=document.getElementById('title')
+console.log(heading);
+
+heading.textContent='hello welcome'
+
+let description=document.querySelector(".desc")
+console.log(description.textContent)
+description.textContent="hiiiiiiiiiiiiiii"
+
---
+++ JAVASCRIPT/FUNCTION/Activities – Functions/Activities.js
@@ -0,0 +1,79 @@
+// //BEGINNER LEVEL
+// //Activity 1: Simple Function
+
+// function greet(){
+//     console.log('Hello Wprld');
+// }
+// greet()
+
+// //Activity 2: Function with Parameter
+
+// function greetUser(name){
+//     console.log("hello "+name);
+    
+// }
+// greetUser("jishnu")
+
+
+//Activity 3: Addition Function
+
+// function add(a,b) {
+//     sum=a+b
+//     console.log(sum);
+    
+// }
+// add(5,4)
+
+//Activity 4: Return a Value
+
+// function square(n) {
+//   return retult=n*n
+
+// }
+// square(5)
+// console.log(retult);
+
+//Activity 5: Even or Odd
+
+
+// function checkOddEven(num) {
+//   let result;
+//     if (num%2===0) {
+//         result='even number'
+//     } else {
+//         result='odd number'
+//     }
+//      return result;
+// }
+// checkOddEven(6)
+// console.log(result);
+
+//Activity 6: Largest Number
+ 
+
+
+// function findLargest(a,b) {
+//     let largest;
+//     if (a>b) {
+//         largest=a
+//     } else {
+//         largest=b
+//     }
+//     return largest
+// }
+// console.log(findLargest(4,6)
+// );
+
+//Activity 7: String Length Counter
+
+// function countCharacters(word) {
+//     let count = 0;
+
+//     for (let char of word) {
+//         count++;
+//     }
+
+//     return count;
+// }
+
+// console.log(countCharacters("hello"));   
